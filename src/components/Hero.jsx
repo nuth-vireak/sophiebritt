e043c6e89c0210757c0d9734f5a77c3f2f243480
nuth-vireak <timberr.4t@gmail.com>
2022-11-01 16:56:08 +0700

    add hero file, updated navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[72px] z-10 flex justify-between px-10'>
-        <div className='flex items-center'>
-            <img src="" alt="" />
-            <h1>Sophie Brittain</h1>
+    <div className='w-full h-[72px] z-10 flex justify-between px-10 bg-white'>
+        <div className='flex items-center hover:cursor-pointer'>
+            <div className='w-4 h-4 bg-orange-600 mr-3 rounded-2xl mt-1 hover:rounded-none hover:bg-blue-500 '></div>
+            <h1 className='text-2xl font-semibold'>Sophie Brittain</h1>
         </div>
         <ul className='flex items-center'>
             <li>Home</li>
