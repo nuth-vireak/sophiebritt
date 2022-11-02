7902ca55366fbed969059ce6b819ed35ac3f2fba
Nuth Vireak <timberr.4t@gmail.com>
2022-11-03 01:32:14 +0700

    Prettier code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import React from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (
-    <div className='w-full h-[72px] z-10 flex justify-between px-10 bg-white'>
-        <div className='flex items-center hover:cursor-pointer'>
-            <div className='w-4 h-4 bg-orange-600 mr-3 rounded-2xl mt-1 hover:rounded-none hover:bg-blue-500 '></div>
-            <h1 className='text-2xl font-semibold'>Sophie Brittain</h1>
+    return (
+        <div className="w-full h-[72px] z-10 flex justify-between px-10 bg-white">
+            <div className="flex items-center hover:cursor-pointer">
+                <div className="w-4 h-4 bg-orange-600 mr-3 rounded-2xl mt-1 hover:rounded-none hover:bg-blue-500 "></div>
+                <h1 className="text-2xl font-semibold">Sophie Brittain</h1>
+            </div>
+            <ul className="flex items-center">
+                <li>Home</li>
+                <li>Work</li>
+                <li>About</li>
+            </ul>
         </div>
-        <ul className='flex items-center'>
-            <li>Home</li>
-            <li>Work</li>
-            <li>About</li>
-        </ul>
-    </div>
-  )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
