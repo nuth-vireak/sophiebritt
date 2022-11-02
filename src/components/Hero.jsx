550d42ca26363b590ab2131155e995375b082c03
Nuth Vireak <timberr.4t@gmail.com>
2022-11-02 23:56:50 +0700

    fuck

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='w-full h-[670px]'>
+      <div>
+      <img src="src\assets\infra.png" alt="Girl in a jacket" width="500" height="600"/>
+        <div>
+          <h1>f</h1>
+          <img src="src\assets\infra.png" alt="sdf" />
+        </div>
+      </div>
+    </div>
   )
 }
 
